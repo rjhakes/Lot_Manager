85f9ad0c4bf7c487121185b53c770507be205c6e
rjhakes <rjhakes@yahoo.com>
2022-01-08 18:11:53 -0700

    init project files

---
+++ server/Tests/APITests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace APITests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ server/Tests/ModelTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace ModelTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
